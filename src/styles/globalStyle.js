8d9987cffc3e229a4a9e1e22183c6f009b091bd9
GabrielNarcizo <eligcan@hotmail.com>
2021-06-15 14:18:52 -0300

    adicionando estilo global a aplicação

---
+++ src/styles/globalStyle.js
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  text-decoration: none;
+  list-style: none;
+
+}
+
+  sub, sup {
+    font-size: inherit;
+    vertical-align: sub;
+    line-height: normal;
+    top: 0;
+    bottom: 0;
+  }
+`;
+
+export default GlobalStyle;
