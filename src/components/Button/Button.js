02d4f0300804bc63b5a155141ac09a8f77c06fca
GabrielNarcizo <eligcan@hotmail.com>
2021-06-23 15:10:01 -0300

    componente de botão adicionado

---
+++ src/components/Button/Button.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Btn from './Button.style'
+
+const Button = () => {
+    return (
+        <div>
+            <Btn>Voltar</Btn>
+        </div>
+    )
+}
+
+export default Button
---
+++ src/components/Button/Button.style.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Btn = styled.button`
+    padding: 10px 20px;
+    margin: 5px 20px;
+    border: 2px solid #00AB63;
+    box-sizing: border-box;
+    border-radius: 4px;
+`
