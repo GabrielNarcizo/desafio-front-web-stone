90ff219fdbe5ba5a2ab361ddc378cfae79bc7ff4
GabrielNarcizo <eligcan@hotmail.com>
2021-06-23 15:55:33 -0300

    hook modificado para context api

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,18 +1,69 @@
-import { useEffect, useState } from 'react'
+import React from 'react'
+import { createContext, useCallback, useContext, useState } from "react";
 
-const useFetch = () => {
+const FetchContext = createContext({});
+
+export const FetchProvider = ({children}) => {
     const [coins, setCoins] = useState([])
+    const [current, setCurrent] = useState({});
 
-    useEffect(() =>{
-        const getCoins = async () => {
-            const response = await fetch('https://economia.awesomeapi.com.br/last/USD-BRL');
-            const data = await response.json()
-            setCoins(data.USDBRL)
-        }
-        getCoins()
+    const [dolar, setDolar] = useState(0)
+    const [tax, setTax] = useState(0)
+    const [result, setResult] = useState(0)
+
+    const [cash, setCash] = useState()
+    const [creditCard, setCreditCard] = useState()
+
+    
+    const getCoins = useCallback(async () => {
+        const response = await fetch('https://economia.awesomeapi.com.br/last/USD-BRL');
+        const data = await response.json()
+        setCoins(data.USDBRL)
     },[])
 
-    return coins
+    const onSubmit = (e) =>{
+        e.preventDefault();
+
+        if(cash) {
+            setResult(((parseFloat(dolar) + parseFloat(tax)) * (parseFloat(coins.low) + parseFloat(1.1))).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }))
+        } else if(creditCard) {
+            setResult((((parseFloat(dolar) + parseFloat(tax)) + parseFloat(6.4)) * parseFloat(coins.low)).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }))
+        } 
+    }
+
+    const setCurrentResults = useCallback((current) => {
+        setCurrent(current);
+      }, []);
+
+    return (
+        <FetchContext.Provider
+            value={{
+                coins,
+                current,
+                setCash,
+                setDolar,
+                setTax,
+                result,
+                cash,
+                creditCard,
+                setCreditCard,
+                onSubmit,
+                setCurrentResults,
+                getCoins
+            }}
+            >
+            {children}
+    </FetchContext.Provider>
+    )
 }
+
+export const useFetch = () => {
+    const fetchContext = useContext(FetchContext);
+    if (!fetchContext) {
+      throw new Error("FetchContext deve ser utilizado dentro de FetchProvider");
+    }
+    return fetchContext;
+  };
+
 
 export default useFetch
